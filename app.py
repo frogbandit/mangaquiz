18e4d44851e68951b3c450c083c221ce7b919483
James Xue <jamesxue100@gmail.com>
2015-04-03 21:01:39 +0100

    made json local for faster

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request, json
 import requests, random
-import urllib
+import simplejson
+import json
 
 app = Flask(__name__)
 
@@ -8,59 +9,67 @@ app.config['DEBUG'] = True
 
 def getTitle():
 
-	list_url = "https://www.mangaeden.com/api/list/0/"
-	list_dict = requests.get(list_url).json();
-	print "length of list: ", len(list_dict.get('manga'))
-	list_num = random.randrange(0, len(list_dict.get('manga')) - 1)
-	print "list_num: ", list_num
-	manga = list_dict.get('manga')[list_num]
-	manga_url =  "https://www.mangaeden.com/api/manga/" + manga.get('i') + "/"
-	print "list url: ", manga_url
+	with open('static/data/mangalist.json') as list_url:
+	# 	print list_url
+	 	list_dict = json.load(list_url)
+	# 	print list_dict
+		#list_dict = open('static/js/data/mangalist.json', "r")
+		#list_dict = requests.get(list_url).json();
+		print "length of list: ", len(list_dict.get('manga'))
+		list_num = random.randrange(0, len(list_dict.get('manga')) - 1)
+		print "list_num: ", list_num
+		manga = list_dict.get('manga')[list_num]
+		manga_url =  "https://www.mangaeden.com/api/manga/" + manga.get('i') + "/"
+		print "list url: ", manga_url
 
-	#manga_dict = requests.get(manga_url).json();
-	chapter_dict = requests.get(manga_url).json();
+		#manga_dict = requests.get(manga_url).json();
+		chapter_dict = requests.get(manga_url).json();
 
-	title = chapter_dict.get('title')
-	return title
+		title = chapter_dict.get('title')
+		return title
 
 def getManga():
 
-	list_url = "https://www.mangaeden.com/api/list/0/"
-	list_dict = requests.get(list_url).json();
-	print "length of list: ", len(list_dict.get('manga'))
-	list_num = random.randrange(0, len(list_dict.get('manga')) - 1)
-	print "list_num: ", list_num
-	manga = list_dict.get('manga')[list_num]
-	manga_url =  "https://www.mangaeden.com/api/manga/" + manga.get('i') + "/"
-	print "list url: ", manga_url
+	with open('static/data/mangalist.json') as list_url:
+		list_dict = json.load(list_url)
+		# list_url = open('static/js/data/mangalist.json', 'w+')
+		#list_url = "https://www.mangaeden.com/api/list/0/"
+		#list_dict = requests.get(list_url).json();
+		#list_dict = open('static/js/data/mangalist.json', "r")
+		print "length of list: ", len(list_dict.get('manga'))
+		list_num = random.randrange(0, len(list_dict.get('manga')) - 1)
+		print "list_num: ", list_num
+		manga = list_dict.get('manga')[list_num]
+		manga_url =  "https://www.mangaeden.com/api/manga/" + manga.get('i') + "/"
+		print "list url: ", manga_url
 
-	#manga_dict = requests.get(manga_url).json();
-	chapter_dict = requests.get(manga_url).json();
-	print "length of manga: ", len(chapter_dict.get('chapters'))
-	chapter_len = len(chapter_dict.get('chapters')) 
-	print "chapter length before ", chapter_len
-	
-	title = chapter_dict.get('title')
+		#manga_dict = requests.get(manga_url).json();
+		chapter_dict = requests.get(manga_url).json();
+		print "length of manga: ", len(chapter_dict.get('chapters'))
+		chapter_len = len(chapter_dict.get('chapters')) 
+		print "chapter length before ", chapter_len
+		
+		title = chapter_dict.get('title')
 
-	if (chapter_len > 0):
-		if  chapter_len == 1:
-			chapter_len = 2
-		print "chapter length after", chapter_len
-		chapter_num = random.randrange(0, chapter_len - 1)
-		chapter = chapter_dict.get('chapters')[chapter_num]
-		chapter_url = "https://www.mangaeden.com/api/chapter/" + chapter[3] + "/"
-		print "chapter url: ", chapter_url
+		if (chapter_len > 0):
+			if  chapter_len == 1:
+				chapter_len = 2
+			print "chapter length after", chapter_len
+			chapter_num = random.randrange(0, chapter_len - 1)
+			chapter = chapter_dict.get('chapters')[chapter_num]
+			chapter_url = "https://www.mangaeden.com/api/chapter/" + chapter[3] + "/"
+			print "chapter url: ", chapter_url
 
-		image_dict = requests.get(chapter_url).json();
-		print "length of chapter: ", len(image_dict.get('images'))
-		image_num = random.randrange(0, len(image_dict.get('images')) - 1)
-		image = image_dict.get('images')[image_num]
-		image_url = "https://cdn.mangaeden.com/mangasimg/" + image[1]
-		print "image url: ", image_url
-		return {'title': title, 'image': image_url}
+			image_dict = requests.get(chapter_url).json();
+			print "length of chapter: ", len(image_dict.get('images'))
+			image_num = random.randrange(0, len(image_dict.get('images')) - 1)
+			image = image_dict.get('images')[image_num]
+			image_url = "https://cdn.mangaeden.com/mangasimg/" + image[1]
+			print "image url: ", image_url
+			return {'title': title, 'image': image_url}
 
-	else:
-		return getManga()
+		else:
+			return getManga()
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
